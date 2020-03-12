0e44d7091077389b2e3f25dc5cf9aa9432b90aa6
NicklasTegner <NicklasMCHD@live.dk>
2020-03-12 23:43:26 +0000

    Build version 1.0.0a4 on mac.

--- src/__required_imports__.py
+++ src/__required_imports__.py
@@ -8,36 +8,36 @@ from core import variables
 from ui.account_manager import *
 from ui.main_ui import *
 from ui import updater
-import wx.lib.dialogs
-import textwrap
-import wx.adv
-import sys
-import threading
-import random
-import time
-import platform
-import subprocess
-import os
-import requests
-from justupdate.client.client import JustUpdateClient
-from justupdate.repo.version import Version
-from core.client_config import ClientConfig
-from datetime import datetime
-import requests_cache
-from reddit_managers.stream_manager import SubredditStreamer
-from ui import preferences
-from ui import info_box
-from reddit_managers import oauth_manager
 import logging
 import pickle
+import sys
+import os
 import lzma
 from Cryptodome.Cipher import AES
 from Cryptodome.Hash import SHA256
 from Cryptodome import Random
+import requests
+import requests_cache
+import logzero
 from appdirs import user_data_dir
 from core.version_helper import git_tag_version, git_tag_release_channel
-import logzero
+import threading
 import http.server
 import socketserver
 import urllib
 import webbrowser
+from reddit_managers import oauth_manager
+import wx.lib.dialogs
+import textwrap
+import platform
+from datetime import datetime
+from reddit_managers.stream_manager import SubredditStreamer
+from ui import preferences
+from ui import info_box
+import wx.adv
+import random
+import time
+import subprocess
+from justupdate.client.client import JustUpdateClient
+from justupdate.repo.version import Version
+from core.client_config import ClientConfig
